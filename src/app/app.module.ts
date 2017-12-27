c9f933d1bd053e17e1146e173bbe08d1db6d4625
Stanley Eosakul <stanley.eosakul@gmail.com>
2017-12-27 17:22:19 -0500

    added Forms, HTTPClient, and Router modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 
+// const appRoutes: Routes = [
+//   { path: '', component: HomeComponent },
+//   { path: 'register', component: RegisterComponent },
+//   { path: 'login', component: LoginComponent },
+//   { path: '**', component: NotFoundComponent }
+// ];
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule.withServerTransition({ appId: 'serverApp' })
+    BrowserModule.withServerTransition({ appId: 'serverApp' }),
+    FormsModule,
+    HttpClientModule,
+    // RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
